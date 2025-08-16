07e01e1497a9880d5fd75364a337485641b973ee
Htet Paing Linn <htetpainglinn2822004@gmail.com>
2025-08-17 02:48:50 +0630

    hpl blockchain update admin approve

--- .history/app/api/cv/validation-request/route_20250817010424.js
+++ .history/app/api/cv/validation-request/route_20250817010424.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI || "mongodb+srv://htetpaing:htetpaing@cluster0.mongodb.net/ForCVs?retryWrites=true&w=majority";
 
 export async function POST(request) {
   let client;
